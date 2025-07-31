790414827ccd6a1ff7dd211f5b1da10e391c1e32
Klaus Rehnberg <klaus.rehnberg@gmail.com>
2025-07-31 15:26:57 +0300

    Fixes #7: Update documentation and Playwright config (#8)

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,24 +3,16 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./apps",
 
-  // Fail the build on CI if you accidentally left test.only in the source code.
-  forbidOnly: !!process.env.CI,
+  retries: 0,
 
-  // Retry on CI only.
-  retries: process.env.CI ? 2 : 0,
+  workers: 1,
 
-  // Opt out of parallel tests on CI.
-  workers: process.env.CI ? 1 : undefined,
-
-  // Reporter to use
-  reporter: "html",
+  reporter: [['list', { printSteps: true }]],
 
   use: {
-    // Collect trace when retrying the failed test.
     trace: "on-first-retry",
   },
 
-  // Configure projects for major browsers.
   projects: [
     {
       name: "chromium",
